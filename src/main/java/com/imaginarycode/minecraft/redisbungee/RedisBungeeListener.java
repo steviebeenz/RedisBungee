49fe790409e5cd3f71600c55bbbb5ef7bb665fac
MacacoLew <72635645+MacacoLew@users.noreply.github.com>
2020-10-18 08:50:32 -0400

    Update RedisBungeeListener.java

--- src/main/java/com/imaginarycode/minecraft/redisbungee/RedisBungeeListener.java
+++ src/main/java/com/imaginarycode/minecraft/redisbungee/RedisBungeeListener.java
@@ -156,7 +156,7 @@ public class RedisBungeeListener implements Listener {
             return;
         }
         
-        if ((event.getTag().equals("legacy:RedisBungee") || event.getTag().equals("RedisBungee")) && event.getSender() instanceof Server) {
+        if ((event.getTag().equals("legacy:redisbungee") || event.getTag().equals("RedisBungee")) && event.getSender() instanceof Server) {
             final String currentChannel = event.getTag();
             final byte[] data = Arrays.copyOf(event.getData(), event.getData().length);
             plugin.getProxy().getScheduler().runAsync(plugin, new Runnable() {
